9e1d934049e1425b5465282a5ccc64622ad0d344
vagola <vladislavs.golubs@yandex.ru>
2019-12-30 15:21:54 +0300

    Fix derive macro

--- protocol-derive/src/lib.rs
+++ protocol-derive/src/lib.rs
@@ -4,22 +4,21 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(MinecraftPacket)]
+#[proc_macro_derive(Packet)]
 pub fn derive_packet(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-
     let name = input.ident;
 
     let output = quote! {
-        impl minecraft_protocol::Packet for #name {
+        impl crate::Packet for #name {
             type Output = Self;
 
             fn encode<W: Write>(&self, writer: &mut W) -> Result<(), EncodeError> {
-
+                todo!();
             }
 
             fn decode<R: Read>(reader: &mut R) -> Result<Self::Output, DecodeError> {
-
+                todo!();
             }
         }
     };
