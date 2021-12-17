12f1086f307a63c1deaeb573454b818b7d20e467
Markus Reiter <me@reitermark.us>
2021-12-17 16:49:30 +0100

    Add `Packet` with custom `encode`/`decode` methods.

--- protocol/src/packet.rs
+++ protocol/src/packet.rs
@@ -1,56 +1,83 @@
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 
 use flate2::read::ZlibDecoder;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
+use minecraft_protocol_derive::{Decoder, Encoder};
 
 use crate::decoder::{Decoder, DecoderReadExt};
 use crate::encoder::{Encoder, EncoderWriteExt};
 use crate::error::{DecodeError, EncodeError};
 
-fn read_n<R: Read>(reader: R, len: usize) -> Result<Vec<u8>, DecodeError> {
-    let mut buf = Vec::with_capacity(len);
-    let bytes_read = reader.take(len as u64).read_to_end(&mut buf)?;
-
-    if bytes_read != len {
-        return Err(DecodeError::Incomplete {
-            bytes_needed: len - bytes_read,
-        });
-    }
-
-    Ok(buf)
-}
-
 #[derive(Debug, Clone)]
-pub struct RawPacket {
+pub struct Packet {
     pub id: i32,
     pub data: Vec<u8>,
 }
 
-impl Encoder for RawPacket {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<(), EncodeError> {
-        writer.write_var_i32(self.id)?;
-        writer.write_all(&self.data)?;
+impl Packet {
+    pub fn encode<W: Write>(
+        &self,
+        writer: &mut W,
+        compression_threshold: Option<i32>,
+    ) -> Result<(), EncodeError> {
+        let mut buf = Vec::new();
+        let packet = RawPacket {
+            id: self.id,
+            data: self.data.clone(),
+        };
+        if let Some(threshold) = compression_threshold {
+            CompressedRawPacket { packet, threshold }.encode(&mut buf)?;
+        } else {
+            packet.encode(&mut buf)?;
+        }
+
+        writer.write_var_i32(buf.len() as i32)?;
+        writer.write_all(&buf)?;
 
         Ok(())
     }
-}
 
-impl Decoder for RawPacket {
-    type Output = Self;
+    pub fn decode<R: Read>(reader: &mut R, compressed: bool) -> Result<Packet, DecodeError> {
+        let len = match reader.read_var_i32() {
+            Ok(len) => len as usize,
+            Err(DecodeError::IoError { io_error })
+                if io_error.kind() == io::ErrorKind::UnexpectedEof =>
+            {
+                return Err(DecodeError::Incomplete { bytes_needed: 1 })
+            }
+            Err(err) => return Err(err.into()),
+        };
 
-    fn decode<R: Read>(reader: &mut R) -> Result<Self::Output, DecodeError> {
-        let id = reader.read_var_i32()?;
+        let mut buf = Vec::with_capacity(len);
+        let bytes_read = reader.take(len as u64).read_to_end(&mut buf)?;
 
-        let mut data = Vec::new();
-        reader.read_to_end(&mut data)?;
+        if bytes_read != len {
+            return Err(DecodeError::Incomplete {
+                bytes_needed: len - bytes_read,
+            });
+        }
+
+        let RawPacket { id, data } = if compressed {
+            CompressedRawPacket::decode(&mut buf.as_slice())?
+        } else {
+            RawPacket::decode(&mut buf.as_slice())?
+        };
 
         Ok(Self { id, data })
     }
 }
 
+#[derive(Debug, Clone, Encoder, Decoder)]
+struct RawPacket {
+    #[data_type(with = "var_int")]
+    pub id: i32,
+    #[data_type(with = "rest")]
+    pub data: Vec<u8>,
+}
+
 #[derive(Debug, Clone)]
-pub struct CompressedRawPacket {
+struct CompressedRawPacket {
     packet: RawPacket,
     threshold: i32,
 }
@@ -61,36 +88,27 @@ impl Encoder for CompressedRawPacket {
         self.packet.encode(&mut packet_buf)?;
 
         let data_len = packet_buf.len() as i32;
-        let mut packet = Vec::new();
         if self.threshold >= 0 && data_len > self.threshold {
-            packet.write_var_i32(data_len)?;
-            let mut encoder = ZlibEncoder::new(&mut packet, Compression::default());
+            writer.write_var_i32(data_len)?;
+            let mut encoder = ZlibEncoder::new(writer, Compression::default());
             encoder.write_all(&packet_buf)?;
             encoder.finish()?;
         } else {
-            packet.write_var_i32(0)?;
-            packet.write_all(&packet_buf)?;
+            writer.write_var_i32(0)?;
+            writer.write_all(&packet_buf)?;
         };
-
-        writer.write_var_i32(packet.len() as i32)?;
-        writer.write_all(&packet)?;
 
         Ok(())
     }
 }
 
 impl Decoder for CompressedRawPacket {
-    type Output = UncompressedRawPacket;
+    type Output = RawPacket;
 
     fn decode<R: Read>(reader: &mut R) -> Result<Self::Output, DecodeError> {
-        let packet_len = reader.read_var_i32()? as usize;
-        let packet_buf = read_n(reader, packet_len)?;
-
-        let mut reader = &mut packet_buf.as_slice();
-
         let data_len = reader.read_var_i32()? as usize;
         let packet = if data_len == 0 {
-            RawPacket::decode(&mut reader)?
+            RawPacket::decode(reader)?
         } else {
             let mut decompressed = Vec::with_capacity(data_len);
             ZlibDecoder::new(reader).read_to_end(&mut decompressed)?;
@@ -102,46 +120,7 @@ impl Decoder for CompressedRawPacket {
             RawPacket::decode(&mut decompressed.as_slice())?
         };
 
-        Ok(UncompressedRawPacket { packet })
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct UncompressedRawPacket {
-    packet: RawPacket,
-}
-
-impl UncompressedRawPacket {
-    /// Compress the packet if is is bigger than given threshold. A negative threshold disables compression.
-    pub fn compress(self, threshold: i32) -> CompressedRawPacket {
-        CompressedRawPacket {
-            packet: self.packet,
-            threshold,
-        }
-    }
-}
-
-impl Encoder for UncompressedRawPacket {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<(), EncodeError> {
-        let mut packet_buf = Vec::new();
-        self.packet.encode(&mut packet_buf)?;
-
-        writer.write_var_i32(packet_buf.len() as i32)?;
-        writer.write_all(&packet_buf)?;
-
-        Ok(())
-    }
-}
-
-impl Decoder for UncompressedRawPacket {
-    type Output = Self;
-
-    fn decode<R: Read>(reader: &mut R) -> Result<Self::Output, DecodeError> {
-        let packet_len = reader.read_var_i32()? as usize;
-        let packet_buf = read_n(reader, packet_len)?;
-        let packet = RawPacket::decode(&mut packet_buf.as_slice())?;
-
-        Ok(Self { packet })
+        Ok(packet)
     }
 }
 
@@ -172,12 +151,10 @@ mod tests {
         let mut data = Vec::new();
         ping_request.encode(&mut data).unwrap();
 
-        let packet = UncompressedRawPacket {
-            packet: RawPacket { id: 1, data },
-        };
+        let packet = Packet { id: 1, data };
 
         let mut vec = Vec::new();
-        packet.encode(&mut vec).unwrap();
+        packet.encode(&mut vec, None).unwrap();
 
         assert_eq!(vec, ping_request_packet_bytes());
     }
@@ -185,9 +162,7 @@ mod tests {
     #[test]
     fn test_uncompressed_packet_decode() {
         let vec = ping_request_packet_bytes();
-        let packet = UncompressedRawPacket::decode(&mut vec.as_slice())
-            .unwrap()
-            .packet;
+        let packet = Packet::decode(&mut vec.as_slice(), false).unwrap();
 
         assert_eq!(packet.id, 1);
         assert_eq!(packet.data, PING_REQUEST_BYTES);
@@ -202,17 +177,12 @@ mod tests {
         let mut data = Vec::new();
         ping_request.encode(&mut data).unwrap();
 
-        let packet = CompressedRawPacket {
-            packet: RawPacket { id: 1, data },
-            threshold: 0,
-        };
+        let packet = Packet { id: 1, data };
 
         let mut vec = Vec::new();
-        packet.encode(&mut vec).unwrap();
+        packet.encode(&mut vec, Some(0)).unwrap();
 
-        let packet = CompressedRawPacket::decode(&mut vec.as_slice())
-            .unwrap()
-            .packet;
+        let packet = Packet::decode(&mut vec.as_slice(), true).unwrap();
 
         assert_eq!(packet.id, 1);
         assert_eq!(packet.data, PING_REQUEST_BYTES);
